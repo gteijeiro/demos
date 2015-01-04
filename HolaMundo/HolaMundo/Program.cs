d750a14338d7f552e24b72cc7cb15e7e7e08b0f7
Guillermo Teijeiro <gteijeiro@outlook.com>
2015-01-04 13:42:31 -0300

    Hola Mundo
    Código de demostración de Hola Mundo con Visual Studio 2015 Preview.

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Console;
+
+namespace HolaMundo
+{
+    class HolaMundo
+    {
+        public HolaMundo()
+        {
+            this.Nombre = "Guillermo Teijeiro";
+            this.Mensaje = string.Format("Primeras lineas de codigo, mi nombre: {0}.", this.Nombre);
+        }
+
+        string Nombre { get; }
+        string Mensaje { get; }
+        
+
+        static void Main(string[] args)
+        {
+            var holaMundo = new HolaMundo();
+            holaMundo.MostrarMensaje();
+        }
+
+        public async void MostrarMensaje()
+        {        
+            WriteLine(this.Mensaje);
+            ReadLine();
+            await Task.Yield();
+        }
+    }
+}
